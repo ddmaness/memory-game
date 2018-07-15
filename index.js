3ae0c3017f68b1f1709afcc4c996ce13ea24be9c
Donald Maness <ddmaness@gmail.com>
2018-07-15 14:33:10 -0400

    Add styling and animations to shape cards

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const shapes = {
     square: '<div class="card"><div class="square"></div><div class="small-square"></div></div>',
     circle: '<div class="circle-card card"><div class="circle"></div><div class="circle-sun"></div></div>',
-    hollowCircle: '<div class="card"><div class="hollow-circle"></div><div class="hollow-circle hollow-circle-1"></div><div class="hollow-circle hollow-circle-2"></div></div>',
     hollowSquare: '<div class="card"><div class="hollow-rect hollow-rect-1"></div><div class="hollow-rect hollow-rect-2"></div><div class="hollow-rect hollow-rect-3"></div><div class="hollow-rect hollow-rect-4"></div></div>',
+    hollowCircle: '<div class="card"><div class="hollow-circle"></div><div class="hollow-circle hollow-circle-1"></div><div class="hollow-circle hollow-circle-2"></div></div>',
     cone: '<div class="card"><div class="cone-circle"></div><div class="cone-triangle"></div></div>',
     triangle: '<div class="card"><div class="triangle-container"><div class="triangle triangle-1"></div><div class="triangle triangle-2"></div><div class="triangle triangle-3"></div></div></div>',
     x: '<div class="card"><div class="line left"></div><div class="line right"></div><div class="line right"></div><div class="line line-1 visible"></div><div class="line line-2 visible"></div><div class="line line-3 visible"></div><div class="line line-4 visible"></div><div class="line line-5 visible"></div></div>',
